aee9bac205b3b213f6ca3cda00f7dd3b6ee8bce5
Dominik Steindl <dominik.steindl@gmx.de>
2025-10-06 16:00:09 +0200

    finished Distorted Card

--- lib/src/features/startingPage/presentation/jp_choose_snack_screen.dart
+++ lib/src/features/startingPage/presentation/jp_choose_snack_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_jp_design_challenge/icons/my_icons.dart';
 
 class ChooseSnackScreen extends StatefulWidget {
   const ChooseSnackScreen({super.key});
@@ -9,6 +12,7 @@ class ChooseSnackScreen extends StatefulWidget {
 
 class _ChooseSnackScreenState extends State<ChooseSnackScreen> {
   final categories = ["All categories", "Salty", "Sweet", "Delicate"];
+  final products = [];
   int selectedIndex = 1;
   @override
   Widget build(BuildContext context) {
@@ -27,7 +31,7 @@ class _ChooseSnackScreenState extends State<ChooseSnackScreen> {
                 padding: EdgeInsets.only(left: 14.0, top: 5.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  spacing: 20,
+                  spacing: 25,
                   children: [
                     Text(
                       "Choose Your Favorite\nSnack",
@@ -43,77 +47,80 @@ class _ChooseSnackScreenState extends State<ChooseSnackScreen> {
                         children: List.generate(categories.length, (index) {
                           final selected = index == selectedIndex;
                           if (index == 0) {
-                            return Padding(
+                            return Container(
+                              height: 50,
                               padding: const EdgeInsets.symmetric(
-                                horizontal: 0,
+                                horizontal: 15,
                               ),
-                              child: Container(
-                                height: 50,
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 1,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Colors.black12,
+                                    const Color.fromARGB(200, 238, 117, 198),
+                                  ],
                                 ),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Colors.white.withValues(alpha: 0.2),
-                                  border: Border.all(
-                                    color: Colors.white.withValues(alpha: 0.5),
-                                    width: 1,
-                                  ),
-                                ),
-                                child: DropdownButtonHideUnderline(
-                                  child: DropdownButton<String>(
-                                    icon: Icon(
-                                      Icons.keyboard_arrow_down_outlined,
-                                      color: Colors.white,
-                                    ),
-                                    dropdownColor: Colors.black87,
-                                    value: categories[0],
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                    selectedItemBuilder: (context) {
-                                      return categories.map((snackCategory) {
-                                        return SizedBox(
-                                          width: 127,
-                                          child: Row(
-                                            children: [
-                                              Transform.translate(
-                                                offset: Offset(4, 0),
-                                                child: Image.asset("assets/details/Lunch.png", width: 24, height: 24,
-                                                color: Colors.white,),
-                                              ),
-                                              SizedBox(width: 8),
-                                              Text(snackCategory),
 
-                                              // right arrow
-                                            ],
-                                          ),
-                                        );
-                                      }).toList();
-                                    },
-                                    items: categories
-                                        .map(
-                                          (snackCategory) => DropdownMenuItem(
-                                            value: snackCategory,
-                                            child: Text(snackCategory),
-                                          ),
-                                        )
-                                        .toList(),
-                                    onChanged: (value) {
-                                      setState(() {
-                                        selectedIndex = categories.indexOf(
-                                          value!,
-                                        );
-                                      });
-                                    },
+                                border: Border.all(
+                                  color: Colors.white.withValues(alpha: 0.5),
+                                  width: 1,
+                                ),
+                              ),
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton<String>(
+                                  icon: Icon(
+                                    Icons.keyboard_arrow_down_outlined,
+                                    color: Colors.white,
                                   ),
+                                  dropdownColor: Colors.black87,
+                                  value: categories[0],
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  selectedItemBuilder: (context) {
+                                    return categories.map((snackCategory) {
+                                      return SizedBox(
+                                        width: 127,
+                                        child: Row(
+                                          children: [
+                                            Transform.translate(
+                                              offset: Offset(6, 0),
+                                              child: Image.asset(
+                                                "assets/details/Lunch.png",
+                                                width: 24,
+                                                height: 24,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                            SizedBox(width: 8),
+                                            Text(snackCategory),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList();
+                                  },
+                                  items: categories
+                                      .map(
+                                        (snackCategory) => DropdownMenuItem(
+                                          value: snackCategory,
+                                          child: Text(snackCategory),
+                                        ),
+                                      )
+                                      .toList(),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedIndex = categories.indexOf(
+                                        value!,
+                                      );
+                                    });
+                                  },
                                 ),
                               ),
                             );
                           }
                           return Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 5,),
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
                             child: GestureDetector(
                               onTap: () =>
                                   setState(() => selectedIndex = index),
@@ -146,6 +153,157 @@ class _ChooseSnackScreenState extends State<ChooseSnackScreen> {
                             ),
                           );
                         }),
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        // Background image
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+                            child: Image.asset("assets/details/cut_card.png"),
+                          ),
+                        ),
+                        Positioned(
+                          top: 16,
+                          right: 16,
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Color(0xFFE970C4),
+                                size: 16,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                '4.8',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        // Bottom-left button
+                        Positioned(
+                          bottom: 35,
+                          left: 16,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xFFE970C4),
+                                  blurRadius: 12,
+                                  spreadRadius: -4,
+                                  offset: Offset(0, 6),
+                                ),
+                              ],
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                color: Color(0xFFE970C4),
+                                width: 1,
+                              ),
+                              gradient: LinearGradient(
+                                colors: [Color(0xFFBB8DE1), Color(0xFF908CF5)],
+                              ),
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.transparent,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: Text(
+                                'Order Now',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        // Bottom-right burger image
+                        Positioned(
+                          bottom: -15,
+                          right: 0,
+                          child: Image.asset(
+                            'assets/grafiken/Burger_3D.png',
+                            width: 200, // adjust as needed
+                            height: 200,
+                          ),
+                        ),
+
+                        // Top-left Description
+                        Positioned(
+                          left: 16,
+                          top: 16,
+
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Angi\'s Yummy Burger',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Delish vegan burger',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Text(
+                                'that tastes like heaven',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              SizedBox(height: 15),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    WidgetSpan(
+                                      child: Icon(
+                                        MyIcons.jpCurrency,
+                                        color: Colors.white,
+                                        size: 16,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: '13.99',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w800
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'We Recommend',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w900,
                       ),
                     ),
                   ],
